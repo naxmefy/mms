825da70a397ac5f9d0746045697186104cd8cca9
MRW Neundorf <ninevillage@gmail.com>
2024-07-02 23:07:11 +0200

    feat(tui): configure crate as lib and add example struct for app and simple spec

---
+++ crates/mms_tui/src/lib.rs
@@ -0,0 +1,17 @@
+pub struct MMSTuiApplication {}
+impl MMSTuiApplication {
+    fn new() -> Result<MMSTuiApplication, ()> {
+        Ok(MMSTuiApplication {})
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new_without_params() {
+        let result = MMSTuiApplication::new();
+        assert!(result.is_ok(), "should initialize with new");
+    }
+}
